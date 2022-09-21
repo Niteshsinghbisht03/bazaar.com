8d682e6fe9080bb35080dd7c280852ba31560b0d
Nitesh Singh Bisht <niteshsinghbisht03@gmail.com>
2022-09-21 12:02:21 +0530

    page-not-found-image-error

--- src/Pagenotfound.jsx
+++ src/Pagenotfound.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import sadimage from "./pngegg.png";
+import { Link } from "react-router-dom";
 
 function Pagenotfound() {
   return (
-    <div className="p-10 sm:flex justify-center ">
+    <div className="justify-center p-10 sm:flex ">
       <div className="w-[40vw] aspect-square text-center">
-        <img src="/src/pngegg.png " alt="error" className="animate-bounce h-full w-full object-cover" />
-        <h1 className="text-4xl -mt-10 font-bold text-orange-600">Page Not Found!</h1>
+        <img
+          src={sadimage}
+          alt="error"
+          className="object-cover w-full h-full animate-bounce"
+        />
+        <h1 className="-mt-10 text-4xl font-bold text-orange-600">
+          Page Not Found!
+        </h1>
       </div>
       <div className="flex flex-col sm:justify-center gap-2 sm:gap-10 w-[40vw] text-center">
         <div>
-          <h2 className="sm:text-[150px]  text-3xl animate-pulse font-bold">404</h2>
+          <h2 className="sm:text-[150px]  text-3xl animate-pulse font-bold">
+            404
+          </h2>
         </div>
         <div>
           <p className="text-3xl">Aha! You See! You Can Be Wrong! </p>
@@ -18,10 +27,19 @@ function Pagenotfound() {
         </div>
         <div>
           <p className="text-3xl">....either way , you should probably</p>
-          <p className="text-3xl text-slate-800">go back to the <Link to="/" className="bg-orange-600 hover:text-white border-2 border-solid border-black rounded-full text-2xl px-2" >HOME</Link> </p>
+          <p className="text-3xl text-slate-800">
+            go back to the{" "}
+            <Link
+              to="/"
+              className="px-2 text-2xl bg-orange-600 border-2 border-black border-solid rounded-full hover:text-white"
+            >
+              HOME
+            </Link>{" "}
+          </p>
         </div>
       </div>
-    </div>);
+    </div>
+  );
 }
 
 export default Pagenotfound;
